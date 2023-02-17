ff6c796b3e9a39793eb6a4684a430420334921db
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-02-17 03:48:56 -0500

    initialized firebase

--- signal-clone-app/firebase.js
+++ signal-clone-app/firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyCo4vJW5Uhd9A4v2Joo0zv8PwfzL_dkCW8',
   authDomain: 'signal-clone-500c5.firebaseapp.com',
@@ -6,3 +10,16 @@ const firebaseConfig = {
   messagingSenderId: '289266857314',
   appId: '1:289266857314:web:9b2d2c0bb2de9860ef283b',
 };
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
